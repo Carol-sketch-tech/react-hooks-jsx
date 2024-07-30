f16ffa0a6fa98ffe4e42fd2bae01922ef79bb0cd
Caroline <bancroftwanjiru10000@gmail.com>
2024-07-30 21:39:50 +0300

    complete

--- index.js
+++ index.js
@@ -30,7 +30,14 @@ function Tweet(){
     )
 }
 // another example 
-return <div id="tweet" className="tweet_body">{ <p>Whoa!</p>,<img src="google.com"/>}</div>
+return <div id="tweet" className="tweet_body">{ <p>Whoa!</p>,<img src="google.com"/> ,function statement(){
+    if(isHello == true){
+        return" Hello";
+    }else{
+        return "goodbye"
+    }
+}}</div>
+
 // the above code linde is naother way to write JSX syntax.
 
 
@@ -41,3 +48,85 @@ return <div id="tweet" className="tweet_body">{ <p>Whoa!</p>,<img src="google.co
 <p>{cuttentTime}</p>)
 // note that we can use normal javascript in JSX by wrapping vannila javascript using curly braces.
 // the code above.
+
+// JSX works with expression not statemenets 
+// in order for our JSX to be converted into regular javascript the JSX we write must be in the form of a 
+// expression not a statement.
+// for instance the if statments are not valid in JSX.
+return ( <h1 id="header">{if(true){
+    "Hello"
+}else{"GoodBye"}}</h1>)
+// the above code block with an if statement is not a valid JSX.
+// however, the ternary expression does work
+return (<h1 id="header">{true?"Hello":"Goodbye"}</h1>)//this works as a substitue for a if..else statement.
+// this is because  statemens do not have a return value, while expressions do 
+
+// if you want to use is statements you have to express the code in functions.for example
+function Header(){
+    function getHeaderText(isHello){
+        if(isHello){
+            return "Hello";
+        }else {return "Goodbye"}
+    }
+    return <h1 id="header" >{getHeaderText(true)}</h1>
+}
+
+// A component can render another component using JSX.
+function Header(){
+    return <h1>Hello</h1>
+}
+
+function Page(){
+    return(
+        <div>
+            <Header/>
+            <p>Some great Content in here</p>
+        </div>
+    );
+}
+// when writing our HTML elements in JSX the names if the elements should all be in lowecase, 
+// just like we tyoically write normal HTML elements.
+// when we are writing componenets in JSX, the name of the component must be capitalized. this is hiw react can differenciate 
+// <Header/> and the normal <header/>element.
+
+// A component Must return One Jsx ELEMENT.
+function PlainDiv() {
+    return <div>I am one line, so I do not need the parentheses</div>;
+  }
+  
+  const Photo = () => {
+    return (
+      <figure>//this is the one element
+        <img
+          className="image"
+          src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+        />
+        <figcaption>Whoa</figcaption>
+      </figure>
+    );
+  };
+  
+  const Table = () => (
+    <table>// this is the one element.
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+      </tr>
+      <tr>
+        <th>312213</th>
+        <th>Tim Berners-Lee</th>
+      </tr>
+    </table>
+  );
+  
+  function ParentComponent() {
+    return (
+      <main>//this is the one element.
+        <PlainDiv />
+        <Photo />
+        <Table />
+      </main>
+    );
+  }
+//   Each of these are valid components but all if these components have one returned JSX 
+// element that contains everything else.
